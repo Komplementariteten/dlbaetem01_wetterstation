602c42eac651ab63e6d75bf1392797ba6b51b4a2
Komplementariteten <47053492+Komplementariteten@users.norepley.github.com>
2024-01-04 14:49:08 +0100

    Auto commit @04-01-2024 14:49

--- cpu/src/main.rs
+++ cpu/src/main.rs
@@ -1,6 +1,13 @@
-use std::io;
+mod input_reader;
+mod plotter;
+
+use std::str;
 use std::io::{Read, Write};
+use std::process::Command;
+use std::time::Duration;
 use clap::Parser;
+use crate::input_reader::{InputReader, ReadStatus};
+use crate::plotter::plot;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -9,25 +16,39 @@ struct Args{
     tty: String
 }
 
+
+
+
 fn main() {
     let args = Args::parse();
 
     println!("Opening Serial device {}!", args.tty);
 
-    let mut port = match serialport::new(&args.tty, 115200).open() {
+    let mut port = match serialport::new(&args.tty, 115200).timeout(Duration::from_secs(5 * 60)).open() {
         Err(e) => {
             eprintln!("Failed to open \"{}\". Error: {:?}", &args.tty, e);
             ::std::process::exit(1);
         }
         Ok(p) => p
     };
+    let mut reader: InputReader = InputReader::new();
     let mut serial_buf: Vec<u8> = vec![0; 1000];
     loop {
-        match port.read(serial_buf.as_mut_slice()) {
-            Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
-            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-            Err(e) => eprintln!("{:?}", e),
+        let readen = match port.read(serial_buf.as_mut_slice()) {
+            Ok(t) => str::from_utf8(&serial_buf[..t]).unwrap().trim().to_string(),
+            Err(e) => format!("{:?}", e),
+        };
+        match reader.read(readen.as_str()) {
+            ReadStatus::Done => break,
+            ReadStatus::Ok => ()
         }
     }
-    println!("Hello, world!");
+    reader.parse();
+    let result1 = reader.adc_counts(1);
+    let result2 = reader.adc_counts(2);
+    let result3 = reader.adc_counts(3);
+    plot(result1, result2, result3).unwrap();
+
+    let mut out = Command::new("/usr/bin/xdg-open").arg("./plot.png").spawn().expect("can't open image");
+    out.wait().expect("program not exited");
 }
--- mcu/run.cpp
+++ mcu/run.cpp
@@ -1,14 +1,17 @@
-#include <pstl/glue_numeric_defs.h>
-
 #include "daisy_seed.h"
 #include "daisysp.h"
 #include <string>
+#include <array>
+#include <math.h>
 
-#define NUMBER_OF_VALUES 1024
-#define TEST_MEMORY_SIZE (NUMBER_OF_VALUES * 16)
+#define NUMBER_OF_VALUES 550
+#define SIN_POINTS 512
+#define TEST_MEMORY_SIZE (NUMBER_OF_VALUES * 16 * 3)
+
+#define DAC_MAX 4095  // 12 bit
 
 using namespace daisy;
-using namespace daisysp;
+using namespace daisysp; 
 
 DaisySeed hw;
 static size_t value_count = 0;
@@ -17,25 +20,49 @@ const float_t ADC_MAX = 0xFFFF;
 const float_t VEE = 3.3;
 const float_t R_1 = 1000;
 
-void store_value(uint16_t value){
+static std::array<uint16_t, NUMBER_OF_VALUES> SINE_WAVE_LOOKUP;
+
+void calculate_sine_wave_lookup(){
+	for (size_t i = 0; i < (SIN_POINTS - 1); i++)
+	{
+		// We only want the positive half wave
+		float_t v = (DAC_MAX * sin((PI_F * i) / SIN_POINTS));
+		SINE_WAVE_LOOKUP[i] = static_cast<uint16_t>(v);
+		// hw.PrintLine("%3d;%7d:%7.5f", i, SINE_WAVE_LOOKUP[i], v);
+	}
+	SINE_WAVE_LOOKUP[SIN_POINTS - 1] = 0;	
+}
+
+void store_value(uint16_t u1, uint16_t u2, uint16_t u3){
 	int32_t *ram = (int32_t *)0xC0000000;
-	unsigned char const *p = reinterpret_cast<unsigned char const *>(&value);
-	memcpy((sizeof(uint16_t) * value_count) + ram, p, sizeof(uint16_t));
+	unsigned char const *p1 = reinterpret_cast<unsigned char const *>(&u1);
+	memcpy((sizeof(uint16_t) * value_count) + ram, p1, sizeof(uint16_t));
 	value_count++;
+	unsigned char const *p2 = reinterpret_cast<unsigned char const *>(&u2);
+	memcpy((sizeof(uint16_t) * value_count) + ram, p2, sizeof(uint16_t));
+	value_count++;
+	unsigned char const *p3 = reinterpret_cast<unsigned char const *>(&u3);
+	memcpy((sizeof(uint16_t) * value_count) + ram, p3, sizeof(uint16_t));
+	value_count++;
+}
+
+uint16_t read_value(size_t ) {
+
 }
 
 void calculate_voltages_and_temp(){
 	int32_t *ram = (int32_t *)0xC0000000;
-	for (size_t i = 1; i < value_count; i++)
+	for (size_t i = 0; i < value_count; i++)
 	{
+		auto v_type = i % 3;
+		v_type = v_type == 0 ? 3 : v_type;
 		uint16_t result;
 		unsigned char const *p = (unsigned char const *)((sizeof(uint16_t) * i) + ram);
 		memcpy(&result, p, sizeof(uint16_t));
 		// Calculate Voltage
+		uint16_t sin_value_indes = i / 3;
 		float_t v =  VEE * (result / ADC_MAX);
-		// Calculate Resistance
-		float_t t = ((VEE - v) * R_1) / v;
-		hw.PrintLine("%7d;%2.6f;%4.6f", result, v, t);
+		hw.PrintLine("%4d;%1d;%6d;%2.6f;%5d#", i, v_type, result, v, sin_value_indes < SIN_POINTS ? SINE_WAVE_LOOKUP[sin_value_indes]: 0);
 	}
 	value_count = 0;
 }
@@ -43,6 +70,7 @@ void calculate_voltages_and_temp(){
 int main(void)
 {
 	hw.Init();
+	hw.SetLed(true);
 
 	/** Configure and Initialize the DAC */
     DacHandle::Config dac_cfg;
@@ -52,33 +80,50 @@ int main(void)
     dac_cfg.chn        = DacHandle::Channel::ONE;
     hw.dac.Init(dac_cfg);
 
-	bool dac_is_high = false;
-
-	// Wait for Serial connection
-	hw.StartLog(true);
-	hw.SetLed(true);
-	AdcChannelConfig adcConfig;
-	adcConfig.InitSingle(hw.GetPin(21));
-
-	hw.adc.Init(&adcConfig, 1);
+	AdcChannelConfig adc_config[3];
+	adc_config[0].InitSingle(seed::A6);
+	adc_config[1].InitSingle(seed::A5);
+	adc_config[2].InitSingle(seed::A4);
+	hw.adc.Init(adc_config, 3);
 	hw.adc.Start();
-	while(value_count < NUMBER_OF_VALUES) {	
-		System::DelayUs(48);
-		if(dac_is_high){
-			hw.dac.WriteValue(DacHandle::Channel::ONE, 0b0); 
-			dac_is_high = false;
-		}
-		else {
-			// On rising Edge
-			store_value(hw.adc.Get(0));
-			// 4096 ~ 3.3V => 1284 ~ 1V
-			hw.dac.WriteValue(DacHandle::Channel::ONE, 1284);
-			dac_is_high = true;
-		}
-	}
-	// 
-	System::Delay(1000);
-	// done turn of the LED
+
+	Switch start_button;
+	start_button.Init(seed::D26);
+
+
+	bool do_measurement = false;
+	calculate_sine_wave_lookup();
+
 	hw.SetLed(false);
-	calculate_voltages_and_temp();	
+
+	// Don't wait for Serial connection
+	hw.StartLog(false);
+
+	while (1)
+	{
+		start_button.Debounce();
+		do_measurement = start_button.Pressed();
+		
+		if(do_measurement) {
+			hw.SetLed(true);
+			for(int i = 0; i < NUMBER_OF_VALUES; i++){
+				if(i < SIN_POINTS){
+					hw.dac.WriteValue(DacHandle::Channel::ONE, SINE_WAVE_LOOKUP[i]);
+				} else if (i == SIN_POINTS)
+				{
+					hw.dac.WriteValue(DacHandle::Channel::ONE, 0);
+				}
+				store_value(hw.adc.Get(0), hw.adc.Get(1), hw.adc.Get(2));
+				System::DelayUs(10);
+			}
+			
+			calculate_voltages_and_temp();
+			hw.PrintLine("DONE");
+			System::Delay(40);
+			hw.SetLed(false);
+			// Wait 5 seconds
+		}
+		System::Delay(1);
+	}
+	
 }
