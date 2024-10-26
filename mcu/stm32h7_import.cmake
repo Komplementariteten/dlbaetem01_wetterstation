c78adad8d7cb8fff039511466965b5c5a8a5b5f3
Komplementariteten <47053492+Komplementariteten@users.noreply.github.com>
2024-10-26 16:00:06 +0200

    Fixes + Memory Map

--- mcu/stm32h7_import.cmake
+++ mcu/stm32h7_import.cmake
@@ -55,7 +55,7 @@ add_compile_definitions(
 # -Wl,--gc-sections     Perform the dead code elimination.
 # --specs=nano.specs    Link with newlib-nano.
 # --specs=nosys.specs   No syscalls, provide empty implementations for the POSIX system calls.
-set(CMAKE_EXE_LINKER_FLAGS "${MCU} -Wl,--gc-sections --specs=nano.specs --specs=nosys.specs -Wl,-Map=${CMAKE_PROJECT_NAME}.map" CACHE INTERNAL "Linker options")
+set(CMAKE_EXE_LINKER_FLAGS "${MCU} -Wl,--gc-sections --specs=nano.specs --specs=nosys.specs -T../STM32H750IB_flash.lds  -Wl,-Map=${CMAKE_PROJECT_NAME}.map" CACHE INTERNAL "Linker options")
 
 #---------------------------------------------------------------------------------------
 # Set debug/release build configuration Options
